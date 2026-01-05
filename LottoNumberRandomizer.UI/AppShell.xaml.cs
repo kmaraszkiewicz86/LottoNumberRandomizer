ecf6bca2902c3139b6fbb260f891f4a2d3f91944
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-01-05 13:22:15 +0100

    Move to src directory

--- LottoNumberRandomizer.UI/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LottoNumberRandomizer.UI;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
---
+++ src/LottoNumberRandomizer.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LottoNumberRandomizer.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
