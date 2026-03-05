322e9e015df228f3e3b996629ef1214f455f5f58
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2026-03-05 11:15:16 +0100

    Add force respecting display cutouts

--- src/LottoNumberRandomizer.UI/Platforms/Android/MainActivity.cs
+++ src/LottoNumberRandomizer.UI/Platforms/Android/MainActivity.cs
@@ -1,14 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace LottoNumberRandomizer.UI;
 
-[Activity(
-    Theme = "@style/Maui.MainTheme",
-    MainLauncher = true,
-    LaunchMode = LaunchMode.SingleTop,
-    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, 
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Force respecting display cutouts
+        if (OperatingSystem.IsAndroidVersionAtLeast(28))
+        {
+            Window?.Attributes?.LayoutInDisplayCutoutMode = LayoutInDisplayCutoutMode.Default;
+        }
+    }
 }
